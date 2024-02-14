808d68aa80d12cdab451d1707873a0e5aa03bba2
Xantrall <bauksutat@mail.ru>
2024-02-14 21:05:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Constans.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.SqlClient;
+using ConsoleApp6;
+
+namespace ConsoleApp6
+{
+    internal class Constans
+    {
+        public const string DatabaseFile = "InvestFond.sqlite";
+        public const string DatabaseConnectionString = "Data Source=" + DatabaseFile;
+        public const string DatabaseServer = "sqlsrv";
+        public const string DatabaseName = "InvestFond";
+        public const string DatabaseUser = "admin";
+        public const string DatabasePassword = "123";
+        public string connectionString = $"Server= {DatabaseServer};Database= {DatabaseName} ;Uid= {DatabaseUser} ;Pwd={DatabasePassword}; TrustServerCertificate=True;";
+        public string SetTableSqlite()
+        {
+            SqliteConnection sqliteConnection = new SqliteConnection(DatabaseConnectionString);
+            using var command = sqliteConnection.CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            Console.WriteLine("Какую таблицу хотите заполнить?");
+            string nameTable = Console.ReadLine();
+            Console.WriteLine($"name {nameTable}:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"number {nameTable}:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"date {nameTable}(xx-xx-xxxx):");
+            string dates = Console.ReadLine();
+            return $"INSERT INTO {nameTable}(Name, Number, Data) VALUES ( '{name}', {num}, '{dates}')";
+        }
+        public string SetTableMysql()
+        {
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+
+            Console.WriteLine("Какую таблицу хотите заполнить?");
+            string nameTable = Console.ReadLine();
+            Console.WriteLine($"name {nameTable}:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"number {nameTable}:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"date {nameTable} (xx-xx-xxxx):");
+            string dates = Console.ReadLine();
+
+            return $"INSERT INTO {nameTable}(Name, Number, Data) VALUES ( '{name}', {num}, '{dates}')";
+        }
+  
+
+
+    }
+
+
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp6;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.SqlClient;
+Constans C = new Constans();
+//Console.WriteLine("Hello, World!");
+File.Create(path:Constans.DatabaseFile).Close();
+using (SqliteConnection databaseConnection = new SqliteConnection(Constans.DatabaseConnectionString))
+{
+        databaseConnection.Open();
+        using (var command = databaseConnection.CreateCommand())
+        {
+
+            while (true)
+            {
+            Console.WriteLine("Меню\n1.Создать таблицы sqlite\n2.Заполнить таблицы sqlite\n3.Заполнить таблицы Mysql\n4.Нажмите Enter что бы выйти");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "CREATE TABLE Brent (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , Name Text(50), Number INT, Data DATE);" +
+                                          "CREATE TABLE Gold (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , Name Text(50), Number INT, Data DATE);" +
+                                          "CREATE TABLE RTS (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text(50), Number INT, Data DATE);";
+                    command.ExecuteNonQuery();
+                    break;
+                    case 2:
+                        command.CommandText = C.SetTableSqlite();
+                        command.ExecuteNonQuery();
+                        break;
+                    case 3:
+                    using (SqlConnection databaseMyConnection = new SqlConnection(C.connectionString))
+                    {
+                        databaseMyConnection.Open();
+                        using (var Mycommand = databaseMyConnection.CreateCommand())
+                        {
+
+                            Mycommand.CommandText = C.SetTableMysql();
+                            Mycommand.ExecuteNonQuery();
+                        }
+                        databaseMyConnection.Close();
+                    }
+                        break;
+                    case 4:
+                        break;
+
+
+                }
+            }
+
+        }
+    databaseConnection.Close();
+}
